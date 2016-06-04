d011fd0bb19f8cac8e58f013768e5a1ebd3bfbce
Adrián Gallego Castellanos <kugoad@openmailbox.org>
2016-06-04 15:32:24 +0200

    Remove API Keys for security reasons

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import telebot # Librería de la API del bot.
+import DMusicBot
 from telebot import types # Tipos para la API del bot.
 import time # Librería para hacer que el programa que controla el bot no se acabe.
+import os # Library for get the environment variables
  
-TOKEN = '219104614:AAE7MO4Eojv11jgRBDH6DjDo1_LH8wb31LQ' # Nuestro tokken del bot (el que @BotFather nos dió).
+TOKEN = os.environ['DMUSICBOT'] # Nuestro tokken del bot (el que @BotFather nos dió).
  
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+
+@bot.message_handler(commands=['hola'])
+def hola(message):
+	bot.reply_to(message, "hola")
  
 def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
     for m in messages: # Por cada dato 'm' en el dato 'messages'
